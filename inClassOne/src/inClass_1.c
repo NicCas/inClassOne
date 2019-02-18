3fdb2c0a06e3e5a7b14bfde64e33644fd958134d
Nicole C <nicas@sonic.net>
2019-02-17 18:30:20 -0800

    Lec 5, Ex 10

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -1,51 +1,25 @@
-
-
+/*
+ printf("Lecture 5:\n\n"); printf("Example 9:\n\n\n");
+ */
 
 #include <stdio.h>
 #include <string.h>
 
-struct Books {
-   char  title[50];
-   char  author[50];
-   char  subject[100];
-   int   book_id;
+//size allocated is 20 bytes. Regardless of which data you use. This way computer can change it at runtime
+union Data {
+   int i;  //4 bytes
+   float f; //4 bytes
+   char str[20];  //20 bytes
 };
 
-/* function declaration */
-void printBook( struct Books book );
-
 int main( ) {
+
 	printf("Lecture 5:\n\n");
-	printf("Example 9:\n\n\n");
+	printf("Example 10:\n\n\n");
 
-   struct Books Book1;        /* Declare Book1 of type Book */
-   struct Books Book2;        /* Declare Book2 of type Book */
+   union Data data;
 
-   /* book 1 specification */
-   strcpy( Book1.title, "C Programming");
-   strcpy( Book1.author, "Nuha Ali");
-   strcpy( Book1.subject, "C Programming Tutorial");
-   Book1.book_id = 6495407;
-
-   /* book 2 specification */
-   strcpy( Book2.title, "Telecom Billing");
-   strcpy( Book2.author, "Zara Ali");
-   strcpy( Book2.subject, "Telecom Billing Tutorial");
-   Book2.book_id = 6495700;
-
-   /* print Book1 info */
-   printBook( Book1 );
-
-   /* Print Book2 info */
-   printBook( Book2 );
+   printf( "Memory size occupied by data : %d\n", sizeof(data));
 
    return 0;
-}
-
-void printBook( struct Books book ) {
-
-   printf( "Book title : %s\n", book.title);
-   printf( "Book author : %s\n", book.author);
-   printf( "Book subject : %s\n", book.subject);
-   printf( "Book book_id : %d\n", book.book_id);
 }
