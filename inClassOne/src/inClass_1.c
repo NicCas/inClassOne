e32d9b6e94ea862220bd077958a6bf2ec9115989
Nicole C <nicas@sonic.net>
2019-02-17 17:54:07 -0800

    First Commit
    All files

---
+++ inClassOne/src/inClass_1.c
@@ -0,0 +1,32 @@
+//IF, ELSE IF, ELSE - Decision Making
+#include <stdio.h>
+
+int main () {
+
+	printf("Lecture 5:\n");
+	printf("Example 1:\n");
+   /* local variable definition */
+   int a = 100;
+
+   /* check the boolean condition */
+   if( a == 10 ) {
+      /* if condition is true then print the following */
+      printf("Value of a is 10\n" );
+   } else if( a == 20 ) {
+      /* if else if condition is true */
+      printf("Value of a is 20\n" );
+   } else if( a == 30 ) {
+      /* if else if condition is true  */
+      printf("Value of a is 30\n" );
+   } else {
+      /* if none of the conditions is true */
+      printf("None of the values are matching\n" );
+   }
+
+   printf("Exact value of a is: %d\n\n", a );
+
+
+   printf("Example 2:\n");
+
+   return 0;
+}
