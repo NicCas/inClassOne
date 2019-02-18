c4d9c5d96ebe026cf3901a0499730749aedcc2d7
Nicole C <nicas@sonic.net>
2019-02-17 18:23:41 -0800

    Lec 5, ex 7

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -1,33 +1,45 @@
-#include <stdio.h>
 
-int main () {
+#include <stdio.h>
+#include <string.h>
+
+struct Books {
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+};
+
+int main( ) {
 
 	printf("Lecture 5:\n\n");
-	printf("Example 6:\n\n\n");
+	printf("Example 7:\n\n\n");
 
-	int  var = 20;
-	   int x = 10;
-	   int  *ip;        /* pointer variable declaration */
+   struct Books Book1;        /* Declare Book1 of type Book */
+   struct Books Book2;        /* Declare Book2 of type Book */
 
-	   ip = NULL; // Point to address 0 for safety
+   /* book 1 specification */
+   strcpy( Book1.title, "C Programming");
+   strcpy( Book1.author, "Nuha Ali");
+   strcpy( Book1.subject, "C Programming Tutorial");
+   Book1.book_id = 6495407;
 
-	   ip = &var;  /* Change where you point to. Now pointing to address of var */
+   /* book 2 specification */
+   strcpy( Book2.title, "Telecom Billing");
+   strcpy( Book2.author, "Zara Ali");
+   strcpy( Book2.subject, "Telecom Billing Tutorial");
+   Book2.book_id = 6495700;
 
-	   x = x + *ip; // x = 10 + 20. grabbing data where pointer is pointing is called dereferencing the pointer
+   /* print Book1 info */
+   printf( "Book 1 title : %s\n", Book1.title);
+   printf( "Book 1 author : %s\n", Book1.author);
+   printf( "Book 1 subject : %s\n", Book1.subject);
+   printf( "Book 1 book_id : %d\n", Book1.book_id);
 
-	   ip = &x; //CHANGE WHERE ip points --> point to address of x
+   /* print Book2 info */
+   printf( "Book 2 title : %s\n", Book2.title);
+   printf( "Book 2 author : %s\n", Book2.author);
+   printf( "Book 2 subject : %s\n", Book2.subject);
+   printf( "Book 2 book_id : %d\n", Book2.book_id);
 
-	   //showing pointer calculation
-	   ip = ip + 3;  // address of ip + (3 * sizeof(int)) = address of ip + 12
-
-	   printf("Address of var variable: %x\n", &var  );
-
-	   /* address stored in pointer variable */
-	   printf("Address stored in ip variable: %x\n", ip );
-
-	   /* access the value using the pointer */
-	   printf("Value of *ip variable: %d\n", *ip );
-
-	   return 0;
-
+   return 0;
 }
