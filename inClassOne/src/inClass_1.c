f896f6c5a0189875caafea13adea4da8c344bd9f
Nicole C <nicas@sonic.net>
2019-02-17 19:02:47 -0800

    Lec 6, Ex 6

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -3,14 +3,23 @@
  */
 #include <stdio.h>
 
-int main() {
-	printf("Lecture 6:\n\n"); printf("Example 5:\n\n\n");
+void main() {
 
-   FILE *fp;        //pointer to a file type
+	 printf("Lecture 6:\n\n"); printf("Example 6:\n\n\n");
 
-   fp = fopen("textFile.txt", "w+");
-   fprintf(fp, "This is testing for fprintf...\n");
-   fputs("This is testing for fputs...\n", fp);
+   FILE *fp;
+   char buff[255];
+
+   fp = fopen("textFile.txt", "r");
+   fscanf(fp, "%s", buff);
+   printf("1 : %s\n", buff );
+
+   fgets(buff, 255, (FILE*)fp);
+   printf("2: %s\n", buff );
+
+   fgets(buff, 255, (FILE*)fp);
+   printf("3: %s\n", buff );
    fclose(fp);
-   return 0;
+
 }
+
