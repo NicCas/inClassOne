4e96f6843326c3790a249bd6a5a012e0b6b028c3
Nicole C <nicas@sonic.net>
2019-02-17 18:32:33 -0800

    Lec 5, Ex 11

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -1,25 +1,29 @@
 /*
- printf("Lecture 5:\n\n"); printf("Example 9:\n\n\n");
+ printf("Lecture 5:\n\n"); printf("Example 10:\n\n\n");
  */
 
 #include <stdio.h>
 #include <string.h>
 
-//size allocated is 20 bytes. Regardless of which data you use. This way computer can change it at runtime
 union Data {
-   int i;  //4 bytes
-   float f; //4 bytes
-   char str[20];  //20 bytes
+   int i;
+   float f;
+   char str[20];
 };
 
 int main( ) {
 
-	printf("Lecture 5:\n\n");
-	printf("Example 10:\n\n\n");
+	printf("Lecture 5:\n\n"); printf("Example 11:\n\n\n");
 
    union Data data;
 
-   printf( "Memory size occupied by data : %d\n", sizeof(data));
+   data.i = 10;
+   data.f = 220.5;  //writes over i
+   strcpy( data.str, "C Programming");  //writes over f
+
+   printf( "data.i : %d\n", data.i);    //print out garbage will read 4 bytes interpret as whole number
+   printf( "data.f : %f\n", data.f);    //print out garbage will read 4 bytes intrepret as
+   printf( "data.str : %s\n", data.str); //print out "C Programming"
 
    return 0;
 }
