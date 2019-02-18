b9bfff676ae3f1e2a56a42c2869fc41e8901589a
Nicole C <nicas@sonic.net>
2019-02-17 18:07:57 -0800

    Lec 5, ex 3

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -3,33 +3,28 @@
 int main () {
 
 	printf("Lecture 5:\n");
-	printf("Example 1:\n");
-   /* local variable definition */
-   int a = 100;
+	printf("Example 3:\n");
+	char grade = 'B';
 
-   /* check the boolean condition */
-   if( a == 10 ) {
-      /* if condition is true then print the following */
-      printf("Value of a is 10\n" );
-   } else if( a == 20 ) {
-      /* if else if condition is true */
-      printf("Value of a is 20\n" );
-   } else if( a == 30 ) {
-      /* if else if condition is true  */
-      printf("Value of a is 30\n" );
-   } else {
-      /* if none of the conditions is true */
-      printf("None of the values are matching\n" );
-   }
+	switch(grade) {
+		case 'A' :
+			printf("Excellent!\n" );
+			break;
+		case 'B' :
+		case 'C' :
+			printf("Well done\n" );
+			break;
+		case 'D' :
+			printf("You passed\n" );
+			break;
+		case 'F' :
+			printf("Better try again\n" );
+			break;
+		default :
+			printf("Invalid grade\n" );
+	}
 
-   printf("Exact value of a is: %d\n\n", a );
+	printf("Your grade is  %c\n", grade );
 
-
-   printf("Example 2:\n");
-   int c , d;
-   c = 10;
-   printf( "Value of d is %d\n", (c == 1) ? 20: 30 );
-   printf( "Value of d is %d\n\n", (c == 10) ? 20: 30 );
-
-   return 0;
+	return 0;
 }
