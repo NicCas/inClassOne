ace834d31b3a4531c4c41a8cdfd65d9f78715273
Nicole C <nicas@sonic.net>
2019-02-17 18:28:20 -0800

    Lec 5, Ex 9

--- inClassOne/src/inClass_1.c
+++ inClassOne/src/inClass_1.c
@@ -1,4 +1,6 @@
 
+
+
 #include <stdio.h>
 #include <string.h>
 
@@ -10,10 +12,11 @@ struct Books {
 };
 
 /* function declaration */
-void printBook( struct Books *book );   //INPUT a pointer
+void printBook( struct Books book );
+
 int main( ) {
 	printf("Lecture 5:\n\n");
-	printf("Example 8:\n\n\n");
+	printf("Example 9:\n\n\n");
 
    struct Books Book1;        /* Declare Book1 of type Book */
    struct Books Book2;        /* Declare Book2 of type Book */
@@ -30,19 +33,19 @@ int main( ) {
    strcpy( Book2.subject, "Telecom Billing Tutorial");
    Book2.book_id = 6495700;
 
-   /* print Book1 info by passing address of Book1 */
-   printBook( &Book1 );
+   /* print Book1 info */
+   printBook( Book1 );
 
-   /* print Book2 info by passing address of Book2 */
-   printBook( &Book2 );
+   /* Print Book2 info */
+   printBook( Book2 );
 
    return 0;
 }
 
-void printBook( struct Books *book ) {
+void printBook( struct Books book ) {
 
-   printf( "Book title : %s\n", book->title);
-   printf( "Book author : %s\n", book->author);
-   printf( "Book subject : %s\n", book->subject);
-   printf( "Book book_id : %d\n", book->book_id);
+   printf( "Book title : %s\n", book.title);
+   printf( "Book author : %s\n", book.author);
+   printf( "Book subject : %s\n", book.subject);
+   printf( "Book book_id : %d\n", book.book_id);
 }
